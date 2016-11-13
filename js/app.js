8de16f71d4c91a48620c9eece2b313a91d55aea2
markmwaura <markkmwaura@gmail.com>
2016-11-14 01:19:25 +0300

    add animation and booking

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+$(function() {
+    $('#gallery a ').on('click', function() {
+
+        var image = $(this).find('img').attr('src');
+
+        var imageselected = image.split('thumbnail');
+        var fullpath = imageselected[0] + "fullsize" + imageselected[1];
+        ///console.log(fullpath);
+        $('#galleryModal #imageContainer').attr('src', fullpath);
+
+    });
+
+
+});
+
 $(document).foundation();
